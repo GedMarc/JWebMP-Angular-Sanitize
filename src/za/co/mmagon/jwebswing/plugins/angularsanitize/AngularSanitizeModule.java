f787f35a3f7ecd7788e16ee209639af1e575476c
Marc Magon <ged_marc@hotmail.com>
2017-06-12 16:31:17 +0200

    first commit

---
+++ src/za/co/mmagon/jwebswing/plugins/angularsanitize/AngularSanitizeModule.java
@@ -0,0 +1,29 @@
+package za.co.mmagon.jwebswing.plugins.angularsanitize;
+
+import za.co.mmagon.jwebswing.base.angular.modules.AngularModuleBase;
+
+/**
+ * The module getting loaded into angular
+ *
+ * @author Marc Magon
+ * @since 08 Jun 2017
+ */
+class AngularSanitizeModule extends AngularModuleBase
+{
+
+    private static final long serialVersionUID = 1L;
+
+    /*
+     * Constructs a new AngularSanitizeModule
+     */
+    public AngularSanitizeModule()
+    {
+        super("ngSanitize");
+    }
+
+    @Override
+    public String renderFunction()
+    {
+        return null;
+    }
+}
---
+++ src/za/co/mmagon/jwebswing/plugins/angularsanitize/AngularSanitizePageConfigurator.java
@@ -0,0 +1,57 @@
+package za.co.mmagon.jwebswing.plugins.angularsanitize;
+
+import za.co.mmagon.jwebswing.Page;
+import za.co.mmagon.jwebswing.PageConfigurator;
+import za.co.mmagon.jwebswing.base.angular.AngularPageConfigurator;
+import za.co.mmagon.jwebswing.plugins.ComponentInformation;
+import za.co.mmagon.jwebswing.plugins.PluginInformation;
+import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
+
+/**
+ *
+ * @author Marc Magon
+ * @since 08 Jun 2017
+ */
+@PluginInformation(pluginName = "Angular Sanitizer",
+        pluginDescription = "The ngSanitize module provides functionality to sanitize HTML.",
+        pluginUniqueName = "jwebswing-angular-sanitizer",
+        pluginVersion = "1.6.4",
+        pluginCategories = "angular,sanitize,$sanitize, ui,web ui, framework",
+        pluginSubtitle = "The ngSanitize module provides functionality to sanitize HTML.",
+        pluginSourceUrl = "https://docs.angularjs.org/api/ngSanitize",
+        pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-Angular-Sanitize/wiki",
+        pluginGitUrl = "https://github.com/GedMarc/JWebSwing-Angular-Sanitize",
+        pluginIconUrl = "",
+        pluginIconImageUrl = "",
+        pluginOriginalHomepage = "https://docs.angularjs.org/api/ngSanitize",
+        pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/AngularSanitize.jar/download"
+)
+@ComponentInformation(name = "Angular Sanitizer",
+        description = "The ngSanitize module provides functionality to sanitize HTML.",
+        url = "https://docs.angularjs.org/api/ngSanitize")
+class AngularSanitizePageConfigurator extends PageConfigurator
+{
+
+    private static final long serialVersionUID = 1L;
+
+    /*
+     * Constructs a new AngularSanitizePageConfigurator
+     */
+    public AngularSanitizePageConfigurator()
+    {
+        //Nothing needed
+    }
+
+    @Override
+    public Page configure(Page page)
+    {
+        if (!page.isConfigured())
+        {
+            JQueryPageConfigurator.setRequired(page.getBody(), true);
+            AngularPageConfigurator.setRequired(page.getBody(), true);
+            page.getBody().addJavaScriptReference(AngularSanitizeReferencePool.AngularSanitize.getJavaScriptReference());
+            page.getAngular().getAngularModules().add(new AngularSanitizeModule());
+        }
+        return page;
+    }
+}
