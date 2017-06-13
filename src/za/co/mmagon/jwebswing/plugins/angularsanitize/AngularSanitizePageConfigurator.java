067d746e44ed21b84aad0909a3795d16ebdebbac
Marc Magon <ged_marc@hotmail.com>
2017-06-13 14:20:08 +0200

    Switch Regular Sanitizer off

--- src/za/co/mmagon/jwebswing/plugins/angularsanitize/AngularSanitizePageConfigurator.java
+++ src/za/co/mmagon/jwebswing/plugins/angularsanitize/AngularSanitizePageConfigurator.java
@@ -1,11 +1,13 @@
 package za.co.mmagon.jwebswing.plugins.angularsanitize;
 
+import java.util.logging.Logger;
 import za.co.mmagon.jwebswing.Page;
 import za.co.mmagon.jwebswing.PageConfigurator;
 import za.co.mmagon.jwebswing.base.angular.AngularPageConfigurator;
 import za.co.mmagon.jwebswing.plugins.ComponentInformation;
 import za.co.mmagon.jwebswing.plugins.PluginInformation;
 import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
+import za.co.mmagon.logger.LogFactory;
 
 /**
  *
@@ -33,6 +35,8 @@ class AngularSanitizePageConfigurator extends PageConfigurator
 {
 
     private static final long serialVersionUID = 1L;
+    private static final Logger log = LogFactory.getLog(AngularSanitizePageConfigurator.class.getName());
+
 
     /*
      * Constructs a new AngularSanitizePageConfigurator
@@ -49,7 +53,20 @@ class AngularSanitizePageConfigurator extends PageConfigurator
         {
             JQueryPageConfigurator.setRequired(page.getBody(), true);
             AngularPageConfigurator.setRequired(page.getBody(), true);
-            page.getBody().addJavaScriptReference(AngularSanitizeReferencePool.AngularSanitize.getJavaScriptReference());
+
+            try
+            {
+                if (Class.forName("za.co.mmagon.jwebswing.plugins.textangular.TextAngularPageConfigurator") != null)
+                {
+                    log.config("Because Text Angular is installed and provides angular sanitize, it will be loaded with Text Angular.");
+                }
+            }
+            catch (ClassNotFoundException ex)
+            {
+                //Only use Angular Sanitize provided if text angular sanitizer module not found
+                page.getBody().addJavaScriptReference(AngularSanitizeReferencePool.AngularSanitize.getJavaScriptReference());
+            }
+
             page.getAngular().getAngularModules().add(new AngularSanitizeModule());
         }
         return page;
