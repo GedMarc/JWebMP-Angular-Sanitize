5dc99d54a2ed5029c630323e57d2948066eb6e25
Marc Magon <ged_marc@hotmail.com>
2018-04-14 08:27:35 +0200

    Prepping for RC1 - CRP Finalizations Slim Scroll

--- src/com/jwebmp/plugins/particlejs/AngularSanitizePageConfigurator.java
+++ src/com/jwebmp/plugins/particlejs/AngularSanitizePageConfigurator.java
@@ -1,11 +1,11 @@
-package za.co.mmagon.jwebswing.plugins.particlejs;
+package com.jwebmp.plugins.particlejs;
 
-import za.co.mmagon.jwebswing.Page;
-import za.co.mmagon.jwebswing.PageConfigurator;
-import za.co.mmagon.jwebswing.base.angular.AngularPageConfigurator;
-import za.co.mmagon.jwebswing.plugins.ComponentInformation;
-import za.co.mmagon.jwebswing.plugins.PluginInformation;
-import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
+import com.jwebmp.Page;
+import com.jwebmp.PageConfigurator;
+import com.jwebmp.base.angular.AngularPageConfigurator;
+import com.jwebmp.plugins.ComponentInformation;
+import com.jwebmp.plugins.PluginInformation;
+import com.jwebmp.plugins.jquery.JQueryPageConfigurator;
 import za.co.mmagon.logger.LogFactory;
 
 import java.util.logging.Level;
@@ -27,12 +27,12 @@ import java.util.logging.Logger;
 		pluginIconUrl = "",
 		pluginIconImageUrl = "",
 		pluginOriginalHomepage = "https://docs.angularjs.org/api/ngSanitize",
-		pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/AngularSanitize.jar/download"
-)
+		pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/AngularSanitize.jar/download")
 @ComponentInformation(name = "Angular Sanitizer",
 		description = "The ngSanitize module provides functionality to sanitize HTML.",
 		url = "https://docs.angularjs.org/api/ngSanitize")
-public class AngularSanitizePageConfigurator extends PageConfigurator
+public class AngularSanitizePageConfigurator
+		extends PageConfigurator
 {
 
 	private static final long serialVersionUID = 1L;
@@ -57,7 +57,7 @@ public class AngularSanitizePageConfigurator extends PageConfigurator
 
 			try
 			{
-				if (Class.forName("za.co.mmagon.jwebswing.plugins.textangular.TextAngularPageConfigurator") != null)
+				if (Class.forName("com.jwebmp.plugins.textangular.TextAngularPageConfigurator") != null)
 				{
 					log.config("Because Text Angular is installed and provides angular sanitize, it will be loaded with Text Angular.");
 				}
@@ -66,10 +66,13 @@ public class AngularSanitizePageConfigurator extends PageConfigurator
 			{
 				log.log(Level.FINER, "Text Angular not found, Placing in Sanitize Reference Pool", ex);
 				//Only use Angular Sanitize provided if text angular sanitizer module not found
-				page.getBody().addJavaScriptReference(AngularSanitizeReferencePool.AngularSanitize.getJavaScriptReference());
+				page.getBody()
+				    .addJavaScriptReference(AngularSanitizeReferencePool.AngularSanitize.getJavaScriptReference());
 			}
 
-			page.getAngular().getAngularModules().add(new AngularSanitizeModule());
+			page.getAngular()
+			    .getAngularModules()
+			    .add(new AngularSanitizeModule());
 		}
 		return page;
 	}
