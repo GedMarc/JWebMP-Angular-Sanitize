d1ca1ee8a1916a561079b8e579d3d24aedff9c7c
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:16 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,7 +1,10 @@
 import com.jwebmp.core.base.angular.services.IAngularModule;
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.angularsanitize.AngularSanitizeModule;
 import com.jwebmp.plugins.angularsanitize.AngularSanitizePageConfigurator;
+import com.jwebmp.plugins.angularsanitize.implementations.AngularSanitizeExclusionsModule;
 
 module com.jwebmp.plugins.angularsanitize {
 	exports com.jwebmp.plugins.angularsanitize;
@@ -10,8 +13,11 @@ module com.jwebmp.plugins.angularsanitize {
 	requires java.logging;
 	requires com.jwebmp.logmaster;
 	requires java.validation;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with AngularSanitizePageConfigurator;
 	provides IAngularModule with AngularSanitizeModule;
 
+	provides IGuiceScanJarExclusions with AngularSanitizeExclusionsModule;
+	provides IGuiceScanModuleExclusions with AngularSanitizeExclusionsModule;
 }
